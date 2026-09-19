5f5e513c596dee53b8a576a1eceb463d43d8b3e4
SiddharthVESIT-glitch <2025.karn.lal@ves.ac.in>
2026-09-19 10:42:40 +0530

    Stack practice

--- Exp1.java
+++ Exp1.java
@@ -1,79 +1,71 @@
 import java.util.Scanner;
 
-public class Exp1 {
-    
-    static int[] stack = new int[5];
-    static int top = -1;
+public class Exp1{
+    static int n=5;
+    static int [] stack = new int [n];
+    static int top=-1;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
         int choice;
 
-        do {
-            System.out.println("------ Simple Stack Program ------");
-            System.out.println("1. Push");
-            System.out.println("2. Pop");
-            System.out.println("3. Peek");
-            System.out.println("4. Display");
-            System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
-            
+        do{
+            System.out.println("----stack program----\n"+
+            "1.push\n"+
+            "2.pop\n"+
+            "3.peek\n"+
+            "4.display\n"+
+            "5.exit");
+
             choice = sc.nextInt();
 
-            if (choice == 1) {
-                if (top == 4) { 
-                    System.out.println("Stack is Full (Overflow)");
-                } 
-                else {
-                    System.out.print("Enter a number to push: ");
+            if (choice==1){
+                if (top ==n-1){
+                    System.out.println("Stack is full");
+                }
+                else{
+                    System.out.println("Enter the element to push");
                     int num = sc.nextInt();
                     top++;
-                    stack[top] = num;
-                    System.out.println(num + " added to stack.");
+                    stack[top]=num;
+                    }
+            }
+            else if (choice==2){
+                if (top==-1){
+                    System.out.println("Stack is empty");
                 }
-                
-            } 
-            else if (choice == 2) {
-                if (top == -1) {
-                    System.out.println("Stack is Empty (Underflow)");
-                } 
-                else {
-                    System.out.println("Popped: " + stack[top]);
+                else{
+                    System.out.println("Popped element is " + stack[top]);
                     top--;
                 }
-                
-            } 
-            else if (choice == 3) {
-                if (top == -1) {
-                    System.out.println("Stack is Empty");
-                } 
-                else {
-                    System.out.println("Top element is: " + stack[top]);
-                }
-                
-            } 
-            else if (choice == 4) {
-                if (top == -1) {
-                    System.out.println("Stack is Empty");
-                } 
-                else {
-                    System.out.print("Stack elements: ");
-                    for (int i = 0; i <= top; i++) {
-                        System.out.print(stack[i] + " ");
-                    }
-                    System.out.println();
-                }
-                
-            } 
-            else if (choice == 5) {
-                System.out.println("Exit");
-            } 
-            else {
-                System.out.println("Invalid choice");
             }
-
-        } while (choice != 5);
-        
+            else if (choice==3){
+                if (top==-1){
+                    System.out.println("Stack is empty");
+                }
+                else{
+                    System.out.println("Stack element top num is :" + stack[top]);
+                }
+            }
+            else if (choice==4){
+                if (top==-1){
+                    System.out.println("Stack is empty");
+                }
+                else{
+                    System.out.println("Stack elements are :");
+                    for (int i=0;i<=top;i++){
+                        System.out.println(stack[i]);
+                    }
+                }
+            }
+            else if (choice==5){
+                System.out.println("Exit");  
+            }
+            else{
+                System.out.println("invalid choice");
+            } 
+        }while(choice!=5);
         sc.close();
     }
 }
+
